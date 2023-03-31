28ca663f31bb9edb05a781ab2167228bec581331
Lucas Sahota <lucaswade4@gmail.com>
2023-03-31 01:31:03 -0700

    Add RSPEC unit tests for product validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it "gives an error with no name" do
+      @category = Category.new(id: 1, name: "Test")
+      @product = Product.new(id: 1, price_cents: 50000, quantity: 100, category: @category)
+      @product.valid?
+      expect(@product.errors.full_messages).to_not be_empty
+    end
+
+    it "gives an error with an invalid price" do
+      @category = Category.new(id: 1, name: "Test")
+      @product = Product.new(id: 1, name: "Gorilla Cage", quantity: 100, category: @category)
+      @product.valid?
+      expect(@product.errors.full_messages).to_not be_empty
+    end
+
+    it "gives an error with an invalid quantity" do
+      @category = Category.new(id: 1, name: "Test")
+      @product = Product.new(id: 1, name: "Gorilla Cage", price_cents: 50000, category: @category)
+      @product.valid?
+      expect(@product.errors.full_messages).to_not be_empty
+    end
+
+    it "gives an error with an invalid category" do
+      @category = Category.new(id: 1, name: "Test")
+      @product = Product.new(id: 1, name: "Gorilla Cage", price_cents: 50000, quantity: 100)
+      @product.valid?
+      expect(@product.errors.full_messages).to_not be_empty
+    end
+
+    it "is a valid product with valid data" do
+      @category = Category.new(id: 1, name: "Test")
+      @product = Product.new(id: 1, name: "Gorilla Cage", price_cents: 50000, quantity: 100, category: @category)
+      expect(@product).to be_valid
+    end
+  end
 end
