7709f01105270f7c205c40ac245cd6270e2d9f97
Lucas Sahota <lucaswade4@gmail.com>
2023-03-31 01:42:22 -0700

    Add user validation to pass tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
 
   has_secure_password
+  validates :email, uniqueness: { case_sensitive: false}, presence:true 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 5 }
+  validates :password_confirmation, presence: true
 
+  
 end
