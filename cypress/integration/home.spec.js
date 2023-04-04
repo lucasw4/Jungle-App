31fbcd1994003fd229418549a631152c8d9abc17
Lucas Sahota <lucaswade4@gmail.com>
2023-04-04 13:52:00 -0700

    Finish home page testing with cypress

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -10,4 +10,8 @@ describe("home page", () => {
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
+
+  it("There is 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
 });
